4c8d8a6ac90ba2ff41f08770c55937f80f05bf08
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-02-05 12:03:30 +0530

    ADD - auto signin

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { Switch, Route } from "react-router-dom";
 import LandingPage from "./Landing/LandingPage";
+import { connect, useDispatch } from 'react-redux';
+import * as actions from '../store/actions/index';
 
 const Main = () => {
+  const dispatch = useDispatch()
+  const autoSignup = useCallback(
+    () => dispatch( actions.authCheckState() ),
+    [dispatch]
+  )
+  useEffect(() => autoSignup());
+
   return (
       <Switch>
         <Route path="/">
@@ -11,4 +20,4 @@ const Main = () => {
       </Switch>
   );
 };
-export default Main;
+export default connect(null, null) (Main);
