0ee2350ba9e82f058fc06bbc6de265c3b4e43d28
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-02-25 20:21:10 +0530

    ADD - token to Authorization header

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,4 +6,7 @@ const instance = axios.create({
     baseURL: BASE_API
 });
 
+const token = localStorage.getItem('token');
+if (token) instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
 export default instance;
