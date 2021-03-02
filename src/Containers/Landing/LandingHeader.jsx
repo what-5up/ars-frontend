6da8f7534f32a3dc7b217c4b7f4f372f1323bab4
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-03-02 15:25:59 +0530

    ADD - booking header

--- src/Containers/Landing/LandingHeader.jsx
+++ src/Containers/Landing/LandingHeader.jsx
@@ -154,6 +154,9 @@ const Menu = ({ direction, isAuthenticated }) => {
     >
       <MenuItems to="/">Home</MenuItems>
       <MenuItems to="/discover">Discover </MenuItems>
+      {!isAuthenticated ? (
+        <MenuItems to="/bookings">Bookings </MenuItems>
+      ) : null}
       <MenuItems to="/contact-us">Contact Us </MenuItems>
       {!isAuthenticated ? <RegisterArea /> : null}
     </Stack>
